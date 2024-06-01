5d825b9aa38d23605461fdf9aabe5119d814ab35
Firman Gustiar <firmansiti15@gmail.com>
2024-06-01 15:03:48 +0700

    crud category

--- app/Controllers/Admin/Category.php
+++ app/Controllers/Admin/Category.php
@@ -22,6 +22,61 @@ class Category extends BaseController
     }
     public function index()
     {
-        //
+        $data = [
+            'data'  => $this->category->paginate('5', 'category'),
+            'title' => 'List Category',
+            'pager' => $this->category->pager,
+        ];
+
+        return view('admin/category/index', $data);
     }
+
+    public function create()
+    {
+        return view('category/create', ['title' => 'Tambah Category']);
+    }
+
+    public function store()
+    {
+        $data = $this->request->getPost();
+
+        if (! $this->validateData($data, $this->rules)) {
+            return redirect()->back()->with('message', $this->validator->getErrors());
+        }
+
+        $this->customer->save($data);
+
+        return redirect()->route('Category::index')->with('message', 'Sukses tambah data');
+    }
+
+    public function edit(int $id)
+    {
+        $data = [
+            'title'    => 'Edit Category',
+            'customer' => $this->category->find($id),
+        ];
+
+        return view('category/edit', $data);
+    }
+
+    public function update(int $id)
+    {
+        $data = $this->request->getPost();
+
+        if (! $this->validateData($data, $this->rules)) {
+            return redirect()->back()->with('message', $this->validator->getErrors());
+        }
+
+        $this->customer->update($id, $data);
+
+        return redirect()->route('Category::index')->with('message', 'Sukses ubah data');
+    }
+
+    public function destroy(int $id)
+    {
+        $this->customer->delete($id);
+
+        return redirect()->back()->with('message', 'Sukses hapus data');
+    }
+
 }
