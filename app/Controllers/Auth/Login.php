7262906f82ec190b9a9a346a002b7e1e0ad031b5
holiq <holiq.ibrahim376@gmail.com>
2024-06-01 13:00:38 +0700

    feat(auth): implement login

--- app/Controllers/Auth/Login.php
+++ app/Controllers/Auth/Login.php
@@ -20,6 +20,36 @@ class Login extends BaseController
     }
     public function index()
     {
-        //
+        return view('auth/login', ['title' => 'Login Admin']);
+    }
+
+    public function process()
+    {
+        $data = $this->request->getPost();
+
+        if (!$this->validateData($data, $this->rules)) {
+            return redirect()->back()->with('errors', $this->validator->getErrors());
+        }
+
+        $check = $this->user->where('username', $data['username'])->first();
+
+        if ($check) {
+            if (password_verify($data['password'], $check->password)) {
+                session()->set('name', $check->name);
+                session()->set('username', $check->username);
+                session()->set('role', $check->role);
+
+                return redirect()->route('Admin\Dashboard::index')->with('message', 'Selamat datang ' . $data['username']);
+            }
+        }
+
+        return redirect()->back()->with('errors', ['Invalid Username or Password!']);
+    }
+
+    public function destroy()
+    {
+        session()->destroy();
+
+        return redirect()->route('Home::index');
     }
 }
---
+++ app/Views/admin/dashboard.php
@@ -0,0 +1,6 @@
+<?= $this->extend('layouts/dashboard') ?>
+<?= $this->section('content') ?>
+<div class="container-xxl my-4">
+    <h1>ok</h1>
+</div>
+<?= $this->endSection() ?>
