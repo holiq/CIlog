a5fdb92707d1a43381bc56d058a0bc344e5bc6e8
marnisah3 <149487386+marnisah3@users.noreply.github.com>
2024-06-01 11:00:44 +0700

    Create Comment.php

---
+++ app/Controllers/Admin/Comment.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers\Admin;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class Comment extends BaseController
+{
+    public function index()
+    {
+       
+    }
+}
