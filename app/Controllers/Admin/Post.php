b70a9abad9ba104bb1d2517f6efb03a1b331a572
marnisah3 <149487386+marnisah3@users.noreply.github.com>
2024-06-01 11:05:45 +0700

    validasi Post.php

--- app/Controllers/Admin/Post.php
+++ app/Controllers/Admin/Post.php
@@ -7,6 +7,21 @@ use CodeIgniter\HTTP\ResponseInterface;
 
 class Post extends BaseController
 {
+    protected $post;
+    protected $rules;
+
+    public function __construct()
+    {
+        $this->post  = new ModelsPost();
+        $this->rules = [
+            'title' => 'required',
+            'slug' => 'required',
+            'category_id' => 'required',
+            'content' => 'required',
+            
+
+        ];
+    }
     public function index()
     {
         
