87e0af286480fa1c31938871a3811e19ebd6f2fc
watimirna <mirnaaa.watii03@gmail.com>
2024-06-01 11:24:03 +0700

    tambah routes user admin

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -12,3 +12,11 @@ $routes->post('/admin/post/store', 'Admin\Post::store');
 $routes->get('/admin/post/edit/(:num)', 'Admin\Post::edit/$1');
 $routes->post('/admin/post/update/(:num)', 'Admin\Post::update/$1');
 $routes->get('/admin/post/delete/(:num)', 'Admin\Post::destroy/$1');
+
+
+$routes->get('/admin/user', 'Admin\User::index');
+$routes->get('/admin/user/create', 'Admin\User::create');
+$routes->post('/admin/user/store', 'Admin\User::store');
+$routes->get('/admin/user/edit/(:num)', 'Admin\User::edit/$1');
+$routes->post('/admin/user/update/(:num)', 'Admin\User::update/$1');
+$routes->get('/admin/user/delete/(:num)', 'Admin\User::destroy/$1');
