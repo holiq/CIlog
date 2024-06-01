c47a757a639f28de538532899bc14a9d25aed05d
marnisah3 <149487386+marnisah3@users.noreply.github.com>
2024-06-01 11:17:46 +0700

    Tambah route post admin

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,9 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/admin/post', 'Admin\Post::index');
+$routes->get('/admin/post/create', 'Admin\Post::create');
+$routes->post('/admin/post/store', 'Admin\Post::store');
+$routes->get('/admin/post/edit/(:num)', 'Admin\Post::edit/$1');
+$routes->post('/admin/post/update/(:num)', 'Admin\Post::update/$1');
+$routes->get('/admin/post/delete/(:num)', 'Admin\Post::destroy/$1');
